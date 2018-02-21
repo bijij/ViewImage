83c4484f3effd714069e846a34fba8ef79cc080d
Joshua Butt <josh.ja.butt@gmail.com>
2018-02-22 04:34:37 +1000

    Add debug info to popup.

--- js/popup.js
+++ js/popup.js
@@ -2,3 +2,11 @@
 document.getElementById('options-page').addEventListener('click', function() {
     chrome.runtime.openOptionsPage();
 });
+
+// Get debug info
+var manifestData = chrome.runtime.getManifest();
+chrome.runtime.getPlatformInfo(function (info) {
+    console.log(info);
+    var debugString = 'v' + manifestData.version + ' (' + info.os + ' ' + info.nacl_arch + ')  - ' + manifestData.current_locale;
+    document.getElementById('debug').innerText = debugString;
+});
