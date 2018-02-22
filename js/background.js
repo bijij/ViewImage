7ce27db912007dc417d69be2541bcf71533291fb
Joshua Butt <josh.ja.butt@gmail.com>
2018-02-23 05:02:12 +1000

    fix background.js

--- js/background.js
+++ js/background.js
@@ -10,7 +10,7 @@ const defaultOptions = {
 };
 
 // Save default options to storage
-chrome.storage.sync.get('defaultOptions', function () {
+chrome.storage.sync.get('defaultOptions', function (storage) {
     if (!storage.defaultOptions) {
         chrome.storage.sync.set({ defaultOptions });
     }
